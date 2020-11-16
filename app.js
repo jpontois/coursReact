ebd2c9256cd78179af1d34c1a0431f397132e755
Oakiyo <oakiyo@MacBook-Pro-de-Oakiyo.local>
2020-11-16 12:48:21 +0100

    Exercice incrément (fonction dans setState)

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-const Welcome = ({name, children}) => (
-    <div>
-        <h1>Bijour {name}</h1>
-        <p>{children}</p>
-    </div>
-)
-
-const App = () => (
-    <div>
-        <Welcome name="Brigitte">
-            Ceci est Brigitte
-        </Welcome>
-        <Welcome name="Madelaine">
-            Madelaine de proustage
-        </Welcome>
-    </div>
-)
-
-class Clock extends React.Component {
+class Incrementer extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            date: new Date()
+            increment: props.start
         }
         this.timer = null
     }
-    
+
     componentDidMount() {
         this.timer = window.setInterval(() => this.tick(), 1000)
     }
@@ -34,18 +16,20 @@ class Clock extends React.Component {
     }
 
     tick() {
-        this.setState({
-            date: new Date()
-        })
+        this.setState((state, props) => (
+            {
+                increment: state.increment + 1
+            }
+        ))
     }
 
     render() {
-        const {date} = this.state
-        return <p>Il est {date .toLocaleDateString()} {date.toLocaleTimeString()} </p>
+        const {increment} = this.state
+        return <div>Ceci est l'incrément : {increment}</div>
     }
 }
 
 ReactDOM.render(
-    <Clock/>,
+    <Incrementer start={10}/>,
     document.querySelector('#app')
 )
